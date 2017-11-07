c1c8fcfc714a0d9449c6fd07f4c377dd9d51d879
Devolution <R@altfeld-im.de>
2017-11-07 23:42:59 +0100

    First release

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# R_pkg_dependencies
-Identifies the fulll package dependencies of all installed R packages
+# Identify R package dependencies
+
+Identifies the full package dependencies of all installed R packages
+
+
+
+# Overview
+
+The script in the file `package_dependencies.R` generates a `data.table`
+in the variable `deps` containing the **dependencies of all installed packages**
+
+1. the required packages for each package (column "requires")
+2. all packages that require a package (column "required_by")
+
+It also shows all packages that are installed in parallel (in different versions).
+
+The dependencies do consider all direct and indirect dependencies (dependency recursion).
+
+
+
+# How to use it
+
+Just source the R script file and view the results in the variable `deps`:
+
+`source("package_dependencies.R")`
+
+
+
+# Use cases
+
+## Estimate the impact before uninstalling package
+
+## Find out why a package was installed implicitly
+
+## Try to find out if you use this package at all (directly or indirectly)
+
+
+
+# License
+
+This R script file is published under the
+GNU LESSER GENERAL PUBLIC LICENSE Version 3, 29 June 2007.
+
+For details see the contained `LICENSE` text file.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ package_dependencies.R
@@ -0,0 +1,75 @@
+library(data.table)
+
+# ***************************************************************************
+# Copyright (C) 2016 Juergen Altfeld (R@altfeld-im.de)
+# ---------------------------------------------------------------------------
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# ***************************************************************************
+
+# ================================================================================================
+#
+# This script generates a data.table in the variable "deps" containing all installed packages and
+# 1. the required packages for each package (column "requires")
+# 2. all packages that require a package (column "required_by")
+#
+# It also shows all packages that are installed in parallel (in different versions).
+#
+# The dependencies do consider all direct and indirect dependencies (dependency recursion).
+#
+# ================================================================================================
+
+# Background information:
+# https://stackoverflow.com/questions/38686427/determine-minimum-r-version-for-all-package-dependencies/38687310?noredirect=1#comment64767951_38687310
+# https://stackoverflow.com/a/40009614/4468078
+# https://stackoverflow.com/questions/14645363/listing-r-package-dependencies-without-installing-packages
+
+
+
+# all installed packages
+avail <- as.data.table(installed.packages())
+
+# extract the relevant columns
+deps <- avail[, .(Package, Version, Depends, Imports, Enhances, LinkingTo)]
+
+# Show duplicated packages (same package installed in different versions)
+duplicated.pkgs <- deps[duplicated(deps$Package), Package]
+deps[Package %in% duplicated.pkgs]
+
+# remove duplicated packages (due to different versions and multiple package locations)
+# and order by package names
+deps <- deps[order(-Version)][, utils::head(.SD, 1), by = .(Package)][order(Package)]   # [Package %in% c("colorspace", "RColorBrewer")]
+
+# Returns a named vector of strings with all (reverse) dependent packages of a package name (vector)
+get.deps <- function(package.names, db, reverse = FALSE) {
+  dep.pkgs <- tools::package_dependencies(package.names,
+                                          db,
+                                          which = c("Depends", "Imports", "Enhances", "LinkingTo"),
+                                          recursive = TRUE,
+                                          reverse = reverse,
+                                          verbose = TRUE)
+  
+  pretty.deb.pkgs <- lapply(dep.pkgs, function(x) {paste(x, collapse = ", " )})
+  
+  return(pretty.deb.pkgs)
+}
+
+# Enrich all dependencies = all required packages (full recursion)
+deps[, requires := get.deps(Package, avail)]
+deps[, required_by := get.deps(Package, avail, reverse = TRUE)]
+
+
+
+# "deps" does now contain the result
+
+
